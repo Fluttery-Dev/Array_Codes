3846f33e30f5bb548d73cf97d5603449470f95fa
Lokesh Mishra <flutterydev@gmail.com>
2022-09-07 10:42:13 +0530

    file 31 and 33 added

--- src/Code31_L.java
+++ src/Code31_L.java
@@ -1,2 +1,27 @@
+import array_2D.Operation_2D_array;
+
+import java.util.Scanner;
+
 public class Code31_L {
+
+    public static void main(String[] args) {
+
+        int n;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the no of rows : ");
+        n = scan.nextInt();
+
+        int[][] arr = new int[n][n];
+        Operation_2D_array.input(arr, scan);
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i][i] != 1) {
+                System.out.println("This is not an Identity Matrix ");
+                return;
+            }
+        }
+
+        System.out.println("This is an Identity Matrix ");
+
+    }
 }
